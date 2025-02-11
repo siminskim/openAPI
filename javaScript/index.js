89a30a0509a190d7fa2e69c5195a7898562c4b82
Michael <siminskim0351@gmail.com>
2025-02-10 20:56:24 -0500

    fetch and filter working

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,4 +1,4 @@
-document.body.style.backgroundColor = 'red'
+// document.body.style.backgroundColor = 'red'
 const apiKey = "live_QCRHWRG1HOZXKIh57YBEFCa2CZ5tn7qUHuxAQSxFK9tbh7dKyElT9D2FNcdXLVZN"
 
 const getRandomDog = async function (params) {
@@ -18,21 +18,26 @@ const getRandomDog = async function (params) {
     
 }
 // getRandomDog()
-const dogCardTemplate = document.querySelector('[data-user-template]')
-
+const dogCardTemplate = document.querySelector('[data-dog-template]')
+const dogCardContainer = document.querySelector('[data-dog-card-container]')
 fetch(`https://api.thedogapi.com/v1/images/search?limit=5&api_key=${apiKey}`)
     .then(response => response.json())
     .then(data => {
         // data = data.filter(img=> img.image?.url!=null)
         data.forEach(dog => {
             
-            // console.log(dog.breeds.length)
+            console.log(dog)
             if(dog.breeds.length >0){
                 const card = dogCardTemplate.content.cloneNode(true).children[0]
                 const header = card.querySelector("[data-header]")
                 const body = card.querySelector("[data-body]")
-                header.textContent = dog.name
-                console.log(dog.breeds[0].name)
+                const dogImg = card.querySelector('[data-dog-image]')
+                dogImg.src = `${dog.url}`
+                header.textContent = dog.breeds[0].name
+                // body.src = dog.url
+                // dogImg.src = `"${dog.url}"`
+                dogCardContainer.append(card)
+                console.log(dog.url)
             }
 
 
