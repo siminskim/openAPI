89370ef8171f1457d203f3acec64d383a615514a
Michael <siminskim0351@gmail.com>
2025-02-12 19:43:16 -0500

    One end point for dog breed and pic

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,74 +1,85 @@
-// document.body.style.backgroundColor = 'red'
-const apiKey = "live_QCRHWRG1HOZXKIh57YBEFCa2CZ5tn7qUHuxAQSxFK9tbh7dKyElT9D2FNcdXLVZN"
-
-// const getRandomDog = async function (params) {
-//     try{
-//         // .com/v1/images/search?api_key=YOUR_API_KEY
-//         let response = await fetch(`  https://api.thedogapi.com/v1/images/search?limit=20&api_key=${apiKey}`)
-//         // console.log(response)
-//         let data = await response.json()
+// const apiKey = "live_QCRHWRG1HOZXKIh57YBEFCa2CZ5tn7qUHuxAQSxFK9tbh7dKyElT9D2FNcdXLVZN"
+// const retrieveBtn = document.getElementById('retrieveBtn')
+// const dogCardTemplate = document.querySelector('[data-dog-template]')
+// const dogCardContainer = document.querySelector('[data-dog-card-container]')
+// retrieveBtn.addEventListener('click', getRandomDog)
+// function getRandomDog(){
+// fetch(`https://api.thedogapi.com/v1/images/search?limit=5&has_breeds=1&api_key=${apiKey}`)
+//     .then(response => response.json())
+//     .then(data => {
 //         console.log(data)
-
-
-//     }catch(error){
-//         console.log(error)
-//     }
-
-
-    
+//         data.forEach(dog => {
+//                 let div = document.createElement('div')
+//                 const para = document.createElement('p')
+//                 const img = document.createElement('img')
+//                 img.src = `${dog.url}`
+//                 img.style.height = '200px'
+//                 img.style.width = '200px'
+//                 div.classList.add('card')
+//                 para.textContent = dog.breeds[0].name
+//                 div.appendChild(para)
+//                 div.appendChild(img)
+//                 dogCardContainer.appendChild(div)
+//         })
+//     })
 // }
-// getRandomDog()
-const retriveBtn = document.getElementById('retriveBtn')
-const dogCardTemplate = document.querySelector('[data-dog-template]')
-const dogCardContainer = document.querySelector('[data-dog-card-container]')
-retriveBtn.addEventListener('click', getRandomDog)
-function getRandomDog(){
-fetch(`https://api.thedogapi.com/v1/images/search?limit=25&has_breeds=1&api_key=${apiKey}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    //    let breeds = data[0]?.breeds[0].name
-        // data = data.filter(img=> img.image?.url!=null)
+// set my api key to a variable so I don't have to put the whole thing inside of the query
+//! Finally figured out how to use template yayay
 
+let apiKey = "live_QCRHWRG1HOZXKIh57YBEFCa2CZ5tn7qUHuxAQSxFK9tbh7dKyElT9D2FNcdXLVZN";
+//selectiong the retrive button
+let retrieveBtn = document.getElementById("retrieveBtn");
+//selecting the html template
+let dogCardTemplate = document.querySelector("[data-dog-template]");
+//selecting the container the cards will go in
+let dogCardContainer = document.querySelector("[data-dog-card-container]");
+//when buttion is clicked getRandomDogs is called
+retrieveBtn.addEventListener("click", getRandomDogs);
 
-        data.forEach(dog => {
-            
-            console.log(dog)
-            // if(dog.breeds.length >0){
-                let div = document.createElement('div')
-                const para = document.createElement('p')
-                const img = document.createElement('img')
-                img.src = `${dog.url}`
-                img.style.height = '200px'
-                img.style.width = '200px'
-                div.classList.add('card')
-                para.textContent = dog.breeds[0].name
-                div.appendChild(para)
-                div.appendChild(img)
-                
-                dogCardContainer.appendChild(div)
+async function getRandomDogs() {
+    //When button is clicked clear the pag for new dogs, this way they don;t just stack on top of each other
+    dogCardContainer.innerHTML = "";
+    //waiting for fetch response comes back
+    let dogs = await fetchDogs();
+    console.log(dogs)
+    // looping through each object in the array
+    dogs.forEach(dog => {
+        //variable to store the breed in. The ? checks to see if breeds exist, if it doesnt it is skipped and no error thrown
+        // the ? is similar to an if statement
+        let breedName = dog.breeds?.[0]?.name;
+        //url for the src=
+        let imgUrl = dog.url
+        //passing data from the fetch 
+        createDogCard(imgUrl, breedName);
+    });
+}
+async function fetchDogs() {
+    try {
+                                    //should only return 6 dogs with the breed
+        let response = await fetch(`https://api.thedogapi.com/v1/images/search?limit=6&has_breeds=1&api_key=${apiKey}`);
 
-
-        //     //     // const card = dogCardTemplate.content.cloneNode(true).children[0]
-        //     //     // const header = card.querySelector("[data-header]")
-        //     //     // const body = card.querySelector("[data-body]")
-        //     //     // const dogImg = card.querySelector('[data-dog-image]')
-        //     //     // dogImg.src = `${dog.url}`
-        //     //     // header.textContent = dog.breeds[0].name
-        //     //     // // body.src = dog.url
-        //     //     // // dogImg.src = `"${dog.url}"`
-        //     //     // // dogCardContainer.append(dogImg)
-        //     //     // dogCardContainer.appendChild(card)
-        //     //     console.log(dog.url)
-            // }
-
-
-        })
-    })
-
+        if (!response.ok) {
+            throw new Error(`API Error: ${response.status}`)};
+            return await response.json();
+    } catch (error) {
+        console.error("Failed to fetch dogs:", error);
+    }
 }
 
+function createDogCard(imgUrl, breedName) {
+    // makes a copy of the html template
+    let card = dogCardTemplate.content.cloneNode(true).children[0];
+//grab the image tag with theclass dog-img
+    let img = card.querySelector(".dog-img");
+    // grabs the p tag with class of dog-breed
+    let para = card.querySelector(".dog-breed");
+//sets the src of the 
+    img.src = imgUrl;
+    // assiging "breed name to the alt attr"
+    img.alt = breedName;
+    //setting dog-brred para to breedName
+    para.textContent = breedName;
 
-
-
-// Pembroke Welsh Corgi, "American Bully" Dutch Shepherd American Pit Bull Terrier "Saluki Cocker Spaniel
+    // Append to container
+    dogCardContainer.appendChild(card);
+}
