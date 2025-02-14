fc2e9bcaea3b85e660cc9868e01314cc4d3dd79d
Michael <siminskim0351@gmail.com>
2025-02-14 18:27:43 -0500

    Second end point added

--- javaScript/index.js
+++ javaScript/index.js
@@ -24,7 +24,6 @@
 //     })
 // }
 // set my api key to a variable so I don't have to put the whole thing inside of the query
-//! Finally figured out how to use template yayay
 
 let apiKey = "live_QCRHWRG1HOZXKIh57YBEFCa2CZ5tn7qUHuxAQSxFK9tbh7dKyElT9D2FNcdXLVZN";
 //selectiong the retrive button
@@ -49,14 +48,15 @@ async function getRandomDogs() {
         let breedName = dog.breeds?.[0]?.name;
         //url for the src=
         let imgUrl = dog.url
+        let id = dog.breeds?.[0]?.id
         //passing data from the fetch 
-        createDogCard(imgUrl, breedName);
+        createDogCard(imgUrl, breedName, id);
     });
 }
 async function fetchDogs() {
     try {
                                     //should only return 6 dogs with the breed
-        let response = await fetch(`https://api.thedogapi.com/v1/images/search?limit=6&has_breeds=1&api_key=${apiKey}`);
+        let response = await fetch(`https://api.thedogapi.com/v1/images/search?limit=6&has_breeds=1&bred_for=1&api_key=${apiKey}`);
 
         if (!response.ok) {
             throw new Error(`API Error: ${response.status}`)};
@@ -66,15 +66,21 @@ async function fetchDogs() {
     }
 }
 
-function createDogCard(imgUrl, breedName) {
+async function createDogCard(imgUrl, breedName, id) {
     // makes a copy of the html template
     let card = dogCardTemplate.content.cloneNode(true).children[0];
 //grab the image tag with theclass dog-img
     let img = card.querySelector(".dog-img");
+    let infoDiv = card.querySelector('.infoDiv')
     // grabs the p tag with class of dog-breed
     let para = card.querySelector(".dog-breed");
+    let moreInfoH3 = card.querySelector(".moreInfoH3")
+    let moreInfoBtn = card.querySelector('.moreInfo')
 //sets the src of the 
     img.src = imgUrl;
+    //set data-more-info 
+    // "data-breed", "Labrador"
+   moreInfoBtn.setAttribute('data-id', `${id}` )
     // assiging "breed name to the alt attr"
     img.alt = breedName;
     //setting dog-brred para to breedName
@@ -82,4 +88,39 @@ function createDogCard(imgUrl, breedName) {
 
     // Append to container
     dogCardContainer.appendChild(card);
+    
+    moreInfoBtn.addEventListener('click', async () => {
+       let response = await fetch(`https://api.thedogapi.com/v1/images/search?breed_id=${id}&api_key=${apiKey}`)
+       console.log(response, "hello")
+        let data = await response.json()
+        moreInfoH3.textContent = "Bred for:"
+        infoDiv.textContent =  data[0].breeds[0].bred_for
+        // let bredFor = data.breeds[0].breed_for
+        // let id = dog.breeds?.[0]?.id
+       console.log(data[0].breeds[0].bred_for)
+    })
+    // console.log(`${id}`)
+//     Array [ {…} ]
+// 0: Object { id: "HkC31gcNm", url: "https://cdn2.thedogapi.com/images/HkC31gcNm_1280.png", width: 300, … }
+// breeds: Array [ {…} ]
+// 0: Object { id: 15, name: "American Pit Bull Terrier", country_code: "US", … }
+// length: 1
+// <prototype>: Array []
+// ​​
+// height: 244
+// ​​
+// id: "HkC31gcNm"
+// ​​
+// url: "https://cdn2.thedogapi.com/images/HkC31gcNm_1280.png"
+// ​​
+// width: 300
 }
+// let moreInfoBtn = ''
+// moreInfoBtn.addEventListener('click', moreInfo()) 
+
+// async function moreInfo() {
+//     let buton = 
+//     // console.log(identifier)
+//     // fetch(`https://api.thedogapi.com/v1/images/search?breed_id=${id}&api_key=${apiKey}`)
+    
+// }
